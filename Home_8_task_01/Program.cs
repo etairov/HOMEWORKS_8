2f6354d4402827100628c7d30cfc3b80411ff8b3
e.tairov <ev.tairov@yandex.ru>
2023-02-19 15:38:32 +0700

    08_1

--- Home_8_task_01/Program.cs
+++ Home_8_task_01/Program.cs
@@ -9,3 +9,82 @@
 8 4 4 2
 */
 //---------------------------------------
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+        }
+    }
+
+    return result;
+}
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int[,] GetRange(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {        
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, j] <= matrix[i, k]) continue;
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }   
+    }
+    return matrix;
+}
+
+int countOfRows = GetNumber("Введите кол-во строк:");
+int countOfColumns = GetNumber("Введите кол-во столбцов:");
+int[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
+Console.WriteLine();
+
+GetRange(matrix);
+PrintMatrix(matrix);

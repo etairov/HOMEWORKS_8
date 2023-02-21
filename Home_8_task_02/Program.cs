1b27e5df6d61d13c114aa824cb748093c1d1ab8e
e.tairov <ev.tairov@yandex.ru>
2023-02-22 02:10:21 +0700

    08_2

--- Home_8_task_02/Program.cs
+++ Home_8_task_02/Program.cs
@@ -14,3 +14,67 @@
 */
 //--------------------------------------------------------------------------------------------------------------
 
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void MinSummRowNumber(int[,] matrix)
+{
+    int index = 0;
+    int minsum = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sumRow = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumRow += matrix[i, j];
+        }
+        Console.WriteLine($"Сумма элементов строки [{i}]: {sumRow}");
+
+        if (i == 0)
+        {
+            minsum = sumRow;
+        }
+        else if (sumRow < minsum)
+        {
+            minsum = sumRow;
+            index = i;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Строка с минимальной суммой элементов равна {index}. ");
+}
+
+int[,] matrix = InitMatrix(7, 3);
+
+PrintMatrix(matrix);
+Console.WriteLine();
+
+MinSummRowNumber(matrix);
+

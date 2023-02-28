27ffa67789aee747c8c9e2dc5a155801036e829e
e.tairov <ev.tairov@yandex.ru>
2023-02-28 14:23:33 +0700

    Home08_3

--- Home_8_task_03/Program.cs
+++ Home_8_task_03/Program.cs
@@ -7,3 +7,71 @@
 15 18
 */
 //--------------------------------
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"({matrix[i, j]}) ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+//------------------------------------
+
+int[,] Multiplication(int[,] matrix, int[,] matrixTwo)
+{
+    int[,] multi = new int[matrix.GetLength(0), matrixTwo.GetLength(1)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixTwo.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrixTwo.GetLength(0); k++)
+            {
+                multi[i, j] += matrix[i, k] * matrixTwo[k, j];
+            }
+        }
+    }
+    return multi;
+}
+
+//------------------------------------
+
+int[,] matrix = InitMatrix(2, 2);
+int[,] matrixTwo = InitMatrix(2, 2);
+int[,] multi = Multiplication(matrix, matrixTwo);
+
+Console.WriteLine($"Заданы матрицы: ");
+Console.WriteLine();
+
+PrintMatrix(matrix);
+Console.WriteLine();
+
+PrintMatrix(matrixTwo);
+Console.WriteLine();
+
+Console.WriteLine($"Матрица- произведение: ");
+Console.WriteLine();
+
+PrintMatrix(multi);
